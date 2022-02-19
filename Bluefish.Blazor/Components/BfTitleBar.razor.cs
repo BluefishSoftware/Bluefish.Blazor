d7000932dcaf5c78c49517ffbbc0e5f3f9f58603
Mark Goldring <markgoldring@hotmail.com>
2022-02-19 10:16:18 +0000

    BfTitleBar - Added AccessCondition parameter

--- Bluefish.Blazor/Components/BfTitleBar.razor.cs
+++ Bluefish.Blazor/Components/BfTitleBar.razor.cs
@@ -2,6 +2,9 @@
 
 public partial class BfTitleBar
 {
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
     [Parameter]
     public RenderFragment ChildContent { get; set; }
 
@@ -13,4 +16,30 @@ public partial class BfTitleBar
 
     [Parameter]
     public string TitleCssClass { get; set; }
+
+    /// <summary>
+    /// Optional function to check whether user has access to the page.
+    /// If returns false then the user will be redirected back to the
+    /// AccessFailUrl address.
+    /// </summary>
+    [Parameter]
+    public Func<bool> AccessCondition { get; set; }
+
+    [Parameter]
+    public string AccessFailUrl { get; set; } = "/";
+
+    protected override void OnInitialized()
+    {
+        try
+        {
+            if(AccessCondition?.Invoke() == false)
+            {
+                NavigationManager.NavigateTo(String.IsNullOrWhiteSpace(AccessFailUrl) ? "/" : AccessFailUrl);
+            }
+        }
+        catch(NavigationException)
+        {
+            // save to ignore exeception
+        }
+    }
 }
